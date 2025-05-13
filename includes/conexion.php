0601b80facd15d657d46ac4dc0e72704cdf67db7
MaaDxd <nicolasmadariaga11@gamil.com>
2025-05-13 15:22:15 -0500

    Se agrega el proyecto

---
+++ includes/conexion.php
@@ -0,0 +1,14 @@
+<?php
+// Conexión
+$servidor = 'localhost';
+$usuario = 'root';
+$password = '';
+$basededatos = 'blog';
+$db = mysqli_connect($servidor, $usuario, $password, $basededatos);
+
+mysqli_query($db, "SET NAMES 'utf8'");//saca se esta haciendo una query y set names es setear los resultados de la base de datos a un utf8 asi q las Ñ van a entrar sin problemas
+
+// Iniciar la sesión
+if(!isset($_SESSION)){
+	session_start();
+}
